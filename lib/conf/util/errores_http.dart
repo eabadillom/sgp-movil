190161dba962a0a37e4a6da1b30bceddbd999861
JulioMoreno1 <158115372+JulioMoreno1@users.noreply.github.com>
2025-08-27 12:13:40 -0600

    Limpiar código innecesario

--- lib/conf/util/errores_http.dart
+++ lib/conf/util/errores_http.dart
@@ -1,30 +1,5 @@
 import 'package:dio/dio.dart';
 
-/*class ErroresHttp {
-  static String verificarCodigoError(DioException e) {
-    final statusCode = e.response?.statusCode;
-    final data = e.response?.data;
-
-    // Si es error 500, mensaje genérico
-    if (statusCode == 500) {
-      return 'Error del servidor. Contacte con el administrador.';
-    }
-
-    // Si viene un JSON con mensajeError
-    if (data is Map<String, dynamic> && data.containsKey('mensajeError')) {
-      return data['mensajeError'];
-    }
-
-    // Si viene como texto plano (por si acaso)
-    if (data is String) {
-      return data;
-    }
-
-    // Fallback
-    return 'Ha ocurrido un error inesperado.';
-  }
-}*/
-
 class ErroresHttp {
   static String obtenerMensajeError(DioException e) {
     // Primero, tratamos errores de red o configuración
